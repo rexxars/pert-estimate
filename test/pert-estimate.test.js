baa9e4545baf02d03bd130cedbbe2ca8b1a7561e
Espen Hovlandsdal <rexxars@gmail.com>
2015-01-18 15:49:12 +0100

    Add basic tests for duration/deviation

---
+++ test/pert-estimate.test.js
@@ -0,0 +1,16 @@
+var test = require('tape');
+var pert = require('../');
+
+test('it gives the correct expected duration', function testDuration(t) {
+    var estimate = pert.expectedDuration(1, 5, 9);
+
+    t.equal(estimate, 30, 'µ=(1+(4*5)+9)/6 should be 30');
+    t.end();
+});
+
+test('it gives the correct standard deviation', function testDeviation(t) {
+    var deviation = pert.standardDeviation(1, 5, 10);
+
+    t.equal(deviation, 1.5, 'σ=(10-1)/6 should be 1.5');
+    t.end();
+});
