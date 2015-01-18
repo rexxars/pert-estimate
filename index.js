f839dbc2641b4136f98c484d00954998209d223f
Espen Hovlandsdal <rexxars@gmail.com>
2015-01-18 15:47:05 +0100

    Provide the module API stubs

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function expectedDuration(optimistic, nominal, pessimistic) {}
+function standardDeviation(optimistic, nominal, pessimistic) {}
+
+exports.expectedDuration = expectedDuration;
+exports.standardDeviation = standardDeviation;
