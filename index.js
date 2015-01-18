31d22f23b65b939350dd1f56061ffbac7555370b
Espen Hovlandsdal <rexxars@gmail.com>
2015-01-18 17:04:14 +0100

    Implement module code

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 'use strict';
 
-function expectedDuration(optimistic, nominal, pessimistic) {}
-function standardDeviation(optimistic, nominal, pessimistic) {}
+function getEstimates() {
+    // Check if this is a O/N/P-object or individual arguments
+    var estimates;
+    if (isNaN(arguments[0]) && arguments[0].pessimistic) {
+        estimates = arguments[0];
+    } else {
+        estimates = {
+            optimistic: arguments[0],
+            nominal: arguments[1],
+            pessimistic: arguments[2]
+        };
+    }
+
+    validateEstimates(estimates);
+    return estimates;
+}
+
+function validateEstimates(estimates) {
+    ['optimistic', 'nominal', 'pessimistic'].forEach(function validateEstimate(estimate) {
+        if (isNaN(estimates[estimate])) {
+            throw new TypeError(estimate + ' estimate must be a number');
+        }
+    });
+}
+
+function expectedDuration(optimistic, nominal, pessimistic) {
+    var estimates = getEstimates(optimistic, nominal, pessimistic);
+
+    // µ = (O + (4 * N) + P) / 6
+    return (estimates.optimistic + (4 * estimates.nominal) + estimates.pessimistic) / 6;
+}
+
+function standardDeviation(optimistic, nominal, pessimistic) {
+    var estimates = getEstimates(optimistic, nominal, pessimistic);
+
+    // σ = (P - O) / 6
+    return (estimates.pessimistic - estimates.optimistic) / 6;
+}
 
 exports.expectedDuration = expectedDuration;
 exports.standardDeviation = standardDeviation;
